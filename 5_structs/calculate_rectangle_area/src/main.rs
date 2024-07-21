b0cca27aa6e8377e7fc872467c31c5540695b6a5
Rhys Hodgson <rhyshodgson@rhys-fedora.home>
2024-07-21 20:58:38 +0100

    complete create_rectangle_area chapter

--- 5_structs/calculate_rectangle_area/src/main.rs
+++ 5_structs/calculate_rectangle_area/src/main.rs
@@ -8,6 +8,33 @@ struct Rectangle {
     height: u32,
 }
 
+// The impl keyword allows us to implement a method on a struct
+// area() must take 'self' as the first param, which represents the struct it has been implemented on
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn can_hold(&self, other_rect: &Rectangle) -> bool {
+        other_rect.width < self.width && other_rect.height < self.height
+    }
+
+    // Here we can also implement a function without the &self parameter
+    // This is no longer considered a method. It is now an 'associated function'.
+    // However, associated functions like this can be used to return a new instance of a type
+    // We can call these functions with the :: syntax, e.g rect1::square(10)
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
     // create a new instance of Rectangle
     let rect1 = Rectangle {
@@ -15,23 +42,26 @@ fn main() {
         height: 50,
     };
 
-    // Using the specifier :? tells println! we want to use an output format called Debug
-    // This allows us to print values that don't implement the Display trait. Which is usable for debugging structs.
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    // Using the specifier :? tells println!() we want to use an output format called Debug
+    // This allows us to print values that don't implement the Display trait.
     // The Display trait allows the value to be easily displayed in the terminal for end user consumption.
     // However, structs don't implement this trait, as it's not clear to Rust how a struct should be printed.
     println!("rect1 is {rect1:?}");
 
     // pass an immutable reference to rect1 into area()
     // which means we can continue to use rect1 in this scope
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
-}
-
-// this function takes an immutable reference to the Rectangle struct
-// we can also see that the width and height are clearly related to eachother
-// as they are both derived from the Rectangle struct
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!("rect1 is {} square pixels.", rect1.area());
 }
