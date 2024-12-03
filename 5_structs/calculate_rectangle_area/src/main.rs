e497328fc06464e7cc1e10de9d9c84e358a3a3ef
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2024-12-03 20:39:34 +0000

    rewrite structs chapter code

--- 5_structs/calculate_rectangle_area/src/main.rs
+++ 5_structs/calculate_rectangle_area/src/main.rs
@@ -1,15 +1,20 @@
-// create a Rectangle struct
-// 'derive' allows us to add custom behavior to our custom types in the form of Traits
-// adding #[derive(Debug)] implements the Debug trait to Rectangle
-// which allows us to print Rectangle in debug mode
+// We use the derive keyword to implement the the Debug trait on our struct
+// This allows us to print out more helpful debugging info.
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
-// The impl keyword allows us to implement a method on a struct
-// area() must take 'self' as the first param, which represents the struct it has been implemented on
+// An 'implementation' block for our Rectangle struct.
+// Everything inside of this impl will be associated with our Rectangle struct.
+// Here, we define area() as a method on the Rectangle struct.
+// Methods take in a parameter 'self' which is a reference to the struct it is being called on.
+// We access methods via dot notation (e.g. foo.area())
+// If we want to change anything about our Rectangle, we can use a mutable reference: &mut self.
+// Functions defined inside of an impl block, that don't take in 'self', are called Associated Functions.
+// Associated functions are often used for constructors that return a new instance of the struct.
+
 impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
@@ -19,14 +24,13 @@ impl Rectangle {
         self.width > 0
     }
 
-    fn can_hold(&self, other_rect: &Rectangle) -> bool {
-        other_rect.width < self.width && other_rect.height < self.height
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
     }
 
-    // Here we can also implement a function without the &self parameter
-    // This is no longer considered a method. It is now an 'associated function'.
-    // However, associated functions like this can be used to return a new instance of a type
-    // We can call these functions with the :: syntax, e.g rect1::square(10)
+    // Associated function.
+    // Self refers to the type of the struct the impl block is for.
+    // We use square() as a constructor function to create a square Rectangle.
     fn square(size: u32) -> Self {
         Self {
             width: size,
@@ -36,32 +40,19 @@ impl Rectangle {
 }
 
 fn main() {
-    // create a new instance of Rectangle
     let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
 
-    let rect2 = Rectangle {
-        width: 10,
-        height: 40,
-    };
+    let square1 = Rectangle::square(30);
 
-    let rect3 = Rectangle {
-        width: 60,
-        height: 45,
-    };
+    if rect1.width() {
+        println!("The rectangle width is {}", rect1.width);
+    }
+    println!("The area of the rectangle is {}", rect1.area());
 
-    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
-    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
-
-    // Using the specifier :? tells println!() we want to use an output format called Debug
-    // This allows us to print values that don't implement the Display trait.
-    // The Display trait allows the value to be easily displayed in the terminal for end user consumption.
-    // However, structs don't implement this trait, as it's not clear to Rust how a struct should be printed.
-    println!("rect1 is {rect1:?}");
-
-    // pass an immutable reference to rect1 into area()
-    // which means we can continue to use rect1 in this scope
-    println!("rect1 is {} square pixels.", rect1.area());
+    if square1.width() {
+        println!("The square width is {}", square1.width);
+    }
 }
