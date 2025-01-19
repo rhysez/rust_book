0f76132fe64140642da0282fb992836c10543d84
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2025-01-19 16:20:22 +0000

    simplify comments

--- 10_generics_traits_lifetimes/traits/src/main.rs
+++ 10_generics_traits_lifetimes/traits/src/main.rs
@@ -1,13 +1,16 @@
 // Traits allow us to define a set of methods that can be used across different types.
-// In this example, we want to define share behaviour between NewsArticle and Tweet.
+// In this example, we want to define shared behaviour between NewsArticle and Tweet.
+// Hint: Remember that there are built-in types in Rust that share traits, like the Copy trait for example!
 
 // We create a new public trait named Summary.
-// Notice that we do not provide a body to our summarize...
-// This is because we do not want to dictate how a given type will implement it.
-// However, we do want to ensure that summarize takes a reference to self, and returns a String.
+// We do not need to provide a function body for summarize().
+// However, if we do provide a function body, this will be treated as the method's default behaviour.
 // In this scenario, any type that implements the Summary trait will gain access to the summarize method.
+
 pub trait Summary {
-    fn summarize(&self) -> String;
+    fn summarize(&self) -> String {
+        String::from("Read more...")
+    }
 }
 
 pub struct NewsArticle {
@@ -16,14 +19,9 @@ pub struct NewsArticle {
     pub content: String
 }
 
-// Now we can implement the Summary trait for our chosen type: NewsArticle.
-// All we have to do is provide the function body
-// AKA, tell Rust what happens when NewsArticle calls summarize.
-impl Summary for NewsArticle {
-    fn summarize(&self) -> String {
-        format!("{}, by {}", self.headline, self.author)
-    }
-}
+// Here we're not providing an implementation for summarize() in NewsArticle.
+// If we were to call NewsArticle.summarize(), it would use the default behaviour (return "Read more...").
+impl Summary for NewsArticle {}
 
 pub struct Tweet {
     pub username: String,
@@ -32,8 +30,8 @@ pub struct Tweet {
     pub retweet: bool
 }
 
-// Then we do the same for Tweet
-// In this case, summarize is handled slightly differently, because it is a tweet.
+// We implement summarize for the Tweet type.
+// In this case, summarize is handled differently, because it is a tweet.
 impl Summary for Tweet {
     fn summarize(&self) -> String {
         format!("{}: {}", self.username, self.content)
